22f9838e22febab4755a350c3b8ef0d0efaceb71
skgur07 <pigjaoki0970@gmail.com>
2024-08-30 16:02:00 +0900

    [Silver V] Title: 소수, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            a = a - (b * (a / b));
+            a *= 10;
+        }
+
+        System.out.println(a / b);
+    }
+}
