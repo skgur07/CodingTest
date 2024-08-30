c0c1d3c4003778fdac965f40cfa2f03225fa52ec
skgur07 <pigjaoki0970@gmail.com>
2024-08-30 16:00:38 +0900

    [Bronze V] Title: 오늘 날짜, Time: 204 ms, Memory: 18508 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Date d = new Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        System.out.println(sdf.format(d));
+    }
+}
