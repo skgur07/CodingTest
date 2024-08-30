89f6020863d95817f4866bf3a1881fc4960fc2b6
skgur07 <pigjaoki0970@gmail.com>
2024-08-30 16:01:19 +0900

    [Bronze I] Title: 약수, Time: 212 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if ( n == 1 ) {
+            int tmp = arr[0] * arr[0];
+            System.out.println(tmp);
+            System.exit(0);
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 0 ; i < arr.length - 1 ; i++) {
+            if (arr[i+1] < min) {
+                min = arr[i+1];
+            }
+        }
+
+        for (int i = 0; i < arr.length - 1 ; i++) {
+            if (max < arr[i+1]) {
+                max = arr[i+1];
+            }
+        }
+
+        System.out.println(min * max);
+
+
+    }
+}
