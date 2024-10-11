45e04d683bf2913fc0e5644bfbda4c56e249034c
skgur07 <pigjaoki0970@gmail.com>
2024-10-11 15:49:57 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 20996 ms, Memory: 128444 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i :
+                arr) {
+            bw.write(i + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
